7fb4d26e4e01d2d48387735738e14f16f46f3d50
MonjedNidal <monjed1010@gmail.com>
2024-01-09 22:28:59 +0200

    styles separated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("./src/tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+    },
   },
   plugins: [],
 };
