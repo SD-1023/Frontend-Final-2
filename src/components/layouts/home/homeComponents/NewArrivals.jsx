2aba4f5cefedd059569aada22595071a1ebdf9ef
DeemaAwwad <145373350+DeemaAwwad@users.noreply.github.com>
2024-01-11 01:06:45 +0200

    new arraival added

--- src/components/layouts/home/homeComponents/NewArrivals.jsx
+++ src/components/layouts/home/homeComponents/NewArrivals.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+
+import { Swiper,SwiperSlide } from 'swiper/react';
+import {Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import Card from '../../../shared-components/Card';
+
+
+
+
 
 export default function NewArrivals() {
   return (
-    <div>
-      NewArrivals
+    <div className='pl-4 my-6'>
+      <h2 className='pb-3'>New Arrivals</h2>
+      
+      <Swiper
+      // modules={[Autoplay]}
+       spaceBetween={16}
+      
+        loop={true}
+      //  autoplay={{
+      //   delay:1000
+      //  }}
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      >
+      
+      <SwiperSlide > 
+      <Card className='w-[8.9375rem] lg:w-[20.73956rem] sm:w-[234px] ' img={'bag1.png'} brand={"Grande"} color={"Blossom Pouch"} price={"$39.49"}/>
+      
+    </SwiperSlide>
+    <SwiperSlide> 
+      <Card className='w-[8.9375rem] lg:w-[20.73956rem] sm:w-[234px] ' img={'bag1.png'} brand={"Grande"} color={"Blossom Pouch"} price={"$39.49"}/>
+      
+    </SwiperSlide>
+    <SwiperSlide> 
+      <Card className='w-[8.9375rem] lg:w-[20.73956rem] sm:w-[234px] ' img={'bag1.png'} brand={"Grande"} color={"Blossom Pouch"} price={"$39.49"}/>
+      
+    </SwiperSlide>
+   
+    <SwiperSlide> 
+      <Card className='w-[8.9375rem] lg:w-[20.73956rem] sm:w-[234px]  ' img={'bag1.png'} brand={"Grande"} color={"Blossom Pouch"} price={"$39.49"}/>
+      
+    </SwiperSlide>
+    <SwiperSlide> 
+      <Card className='w-[8.9375rem] lg:w-[20.73956rem] sm:w-[234px] '  img={'bag1.png'} brand={"Grande"} color={"Blossom Pouch"} price={"$39.49"}/>
+      
+    </SwiperSlide>
+
+      
+   
+      
+
+       
+      </Swiper>
     </div>
   )
 }
