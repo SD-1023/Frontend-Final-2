f45680733b54993c5b5ae029535672959b185b00
MonjedNidal <monjed1010@gmail.com>
2024-01-14 23:27:13 +0200

    bottom nav added

--- src/components/glabal/header/navs/DesktopNavbar.jsx
+++ src/components/glabal/header/navs/DesktopNavbar.jsx
@@ -3,6 +3,10 @@ import Icon from "../../../shared-components/Icon";
 
 export default function DesktopNavbar({ categories }) {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <>
       <div className="flex items-center">
@@ -24,9 +28,7 @@ export default function DesktopNavbar({ categories }) {
             className="bg-color-grey text-sm font-medium rounded w-80 h-[2.75rem] border-0 outline-none pl-10"
             placeholder="Search for products or brands....."
             value={searchTerm}
-            onChange={(e) => {
-              setSearchTerm(e.target.value);
-            }}
+            onChange={handleSearch}
           />
           <Icon
             className="absolute inset-0 left-[0.5rem] top-[0.62rem]"
--- src/components/layouts/AppLayout.jsx
+++ src/components/layouts/AppLayout.jsx
@@ -2,6 +2,7 @@ import HomeLayout from "./home/HomeLayout";
 import Header from "../glabal/header/Header";
 import Footer from "../glabal/footer/Footer";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import BottomNavigation from "../glabal/bottom-nav/BottomNavigation";
 
 export default function AppLayout() {
   return (
@@ -12,6 +13,7 @@ export default function AppLayout() {
           <Route index element={<HomeLayout />} />
         </Routes>
         <Footer />
+        <BottomNavigation />
       </BrowserRouter>
     </div>
   );
