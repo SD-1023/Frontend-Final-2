e302582bfd1c9fb7ee3aad553cd8eca799bb5d42
MonjedNidal <monjed1010@gmail.com>
2024-01-20 21:15:03 +0200

    card text is responsive now

--- src/components/shared-components/Card.jsx
+++ src/components/shared-components/Card.jsx
@@ -7,12 +7,14 @@ export default function Card({ brand, color, price, img, className }) {
     <div className={`relative ${className} `}>
       <img className="rounded-lg" alt="" src={`${IMG_PATH}${img}`} />
       <div className="pt-2">
-        <h4 className="text-xs font-semibold text-color-typeHighEmphasis">
+        <h4 className="text-xs md:text-base font-semibold md:font-medium leading-4 md:leading-5 text-color-typeHighEmphasis">
           {brand}
         </h4>
         <Icon className="absolute right-0 bottom-10 " name="heart" />
-        <p className="text-xs text-color-typeLowEmphasis">{color}</p>
-        <span className="text-sm font-semibold text-color-typeHighEmphasis">
+        <p className="text-xs md:text-sm leading-4 md:leading-5 text-color-typeLowEmphasis">
+          {color}
+        </p>
+        <span className="text-sm md:text-base font-semibold md:font-medium leading-4 text-color-typeHighEmphasis">
           {price}
         </span>
       </div>
